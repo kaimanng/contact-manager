88b0e70a0fdd84eb5a165a195b93c0ebce9ac18c
Kaiman Ng <Kaiman@Kaimans-MacBook-Air.local>
2019-02-09 11:40:34 +0800

    Updates

---
+++ precache-manifest.0a6a8301271831fdfeb6a4022e60fa25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "cd8fdd9488dda2480711",
+    "url": "/contact-manager/static/js/main.cd8fdd94.chunk.js"
+  },
+  {
+    "revision": "9a926209981a5f10c492",
+    "url": "/contact-manager/static/js/1.9a926209.chunk.js"
+  },
+  {
+    "revision": "cd8fdd9488dda2480711",
+    "url": "/contact-manager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "9a926209981a5f10c492",
+    "url": "/contact-manager/static/css/1.56f8a2a2.chunk.css"
+  },
+  {
+    "revision": "ca58fcbd313358e62481b4176799cd0d",
+    "url": "/contact-manager/index.html"
+  }
+];
